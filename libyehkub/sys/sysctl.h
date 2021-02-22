aedf68c4fb2351c7ec78fee7ca5a669309baceb0
yehkub <79363763+yehkub@users.noreply.github.com>
2021-02-21 20:11:40 -0600

    Delete libyehkub directory

--- libyehkub/sys/sysctl.h
+++
@@ -1,9 +0,0 @@
-#ifndef _YEHKUB_SYSCTL_H_
-#define _YEHKUB_SYSCTL_H_
-
-#include <stdio.h>
-
-/* Read one line of length (size_t) idsz from DMI descriptors - (const char) *dmipath */
-int read_sys_info(char *bufout, const char *dmipath, size_t idsz);
-
-#endif
