093e89fe36b28f8cb14c4e591073ae5d5d764b8c
yehkub <79363763+yehkub@users.noreply.github.com>
2021-03-05 23:05:19 +0100

    self modifying binary
    $ objdump -M intel -d a.out
    0000000000001239 <main>:
        1239:       f3 0f 1e fa                     endbr64
        123d:       55                                 push   rbp
        123e:       48 89 e5                       mov    rbp,rsp
        1241:       48 83 ec 20                  sub    rsp,0x20
        1245:       c7 45 e4 04 00 00 00   mov    DWORD PTR [rbp-0x1c],0x4
        124c:       c7 45 e8 02 00 00 00   mov    DWORD PTR [rbp-0x18],0x2
        1253:       8b 55 e4                      mov    edx,DWORD PTR [rbp-0x1c]
        1256:       8b 45 e8                      mov    eax,DWORD PTR [rbp-0x18]
        1259:       01 d0                           add    eax,edx    <main+0x20> # byte 0xd0 is <main+0x21>
    
    $ ./a.out
    [mprotect]: setting rw on 7fefce790000 / sz: 4096
    [0x7fefce79025a - opcode: 0xd0] sz: 1 byte
    [mem]: x = { 4 } | y = { 2 } | (x + y) -> z_value: 6
    [op]: changing opcode 0xd0 to 0xc0 { 'add eax, edx' -> 'add eax, eax' }..
    [0x7fefce79025a - opcode: 0xc0] sz: 1 byte
    [mem]: x = { 4 } | y = { 2 } | (x + y) -> z_value: 4

---
+++ self_modify/self_modify.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>     /* getpagesize() */
+#include <sys/mman.h>	/* mprotect() */
+
+static int rw = 0;
+
+int unlock_text_segment(void *addr) {
+	if (rw == 1) { return 0; }
+
+	int page_size = getpagesize();
+	addr -= (unsigned long)addr % page_size;
+	
+	printf("[mprotect]: setting rw on %lx / sz: %d\n", (unsigned long)addr, page_size);
+	
+	int chmemm = mprotect(addr, page_size, PROT_READ | PROT_WRITE | PROT_EXEC);
+	if (chmemm != 0) {
+ 		printf("[E%d]: failed to set rw on text segment\n", chmemm);
+		return 1;
+	}
+	rw = 1;	
+}	
+
+int main(void) {
+	int x = 4;
+	int y = 2;
+	int eax_val = x + y;
+	
+	unsigned char (*mainptr)() = main;		/* we could set this directly to <main+0x21>, but i needed <main> address, for other things that i removed from here.. */
+	unsigned char *xptr = *mainptr + 0x21;	/* points to [01 d0] 'add eax, edx' , we're gonna change this to: [01 c0] 'add eax, eax' */
+	
+	unlock_text_segment(mainptr);			/* unlock text segment */
+	
+	printf("[%p - opcode: 0x%02hhx] sz: %zu byte\n", xptr, *xptr, sizeof(*xptr));
+	printf("[mem]: x = { %d } | y = { %d } | (x + y) -> z_value: %d\n", x, y, eax_val);
+	if (*xptr == 0xd0) {
+		printf("[op]: changing opcode 0x%02hhx to 0xc0 { 'add eax, edx' -> 'add eax, eax' }..\n", *xptr);
+		*xptr = 0xc0;				/* change edx to eax d0 -> c0 */
+		main();					/* jump to main() */
+	}
+	return 0;
+}
